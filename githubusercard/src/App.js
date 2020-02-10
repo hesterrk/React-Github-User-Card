3731f4c55c7ed09a0208fff03e54bfe2645fb740
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-10 21:58:29 +0000

    setup

---
+++ githubusercard/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+
+    <div className="card">
+    <img />
+    <div className="cardInfo">
+      <h3 className="name"></h3>
+      <p className="username"></p>
+      <p> Location: </p>
+      <p>Profile
+    <a href></a>
+
+      </p>
+      <p>Folllowers:</p>
+      <p>Following:</p>
+      <p>Bio:</p>
+    </div>
+    </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
